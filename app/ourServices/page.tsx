728cf44da7a24e4c5cef2576155299e1f121f77f
omarhassan <omarhassan.mansour@gmail.com>
2026-03-09 00:04:13 +0200

    feat: improves ourServices section style !!!

--- app/ourServices/page.tsx
+++ app/ourServices/page.tsx
@@ -1,76 +1,15 @@
+import OurServices from "../components/pages/OurServices";
 import Pane from "../components/Pane";
 
 function page() {
-  const services = [
-    {
-      title: "مسك الدفاتر",
-      desc: "تنظيم السجلات المالية وتقديم تقارير دقيقة تساعد في اتخاذ قرارات استراتيجية مبنية على بيانات واضحة",
-    },
-    {
-      title: "الزكاة والضرائب",
-      desc: "إدارة التزامات الزكاة والضرائب بكفاءة عالية مع تقديم الاستشارات المتخصصة لضمان الامتثال وتقليل المخاطر",
-    },
-    {
-      title: "المراجعة وتدقيق الحسابات",
-      desc: "ضمان دقة البيانات المالية وتعزيز الثقة في تقاريرك المحاسبية وفقاً للمعايير المحلية والدولية",
-    },
-    {
-      title: "مشاريع البيع على الخارطة",
-      desc: "خدمات محاسبية ومراجعة مخصصة لمشاريع البيع على الخارطة وفق متطلبات الجهات التنظيمية",
-    },
-    {
-      title: "الإجراءات المتفق عليها",
-      desc: "تنفيذ فحوصات مالية محددة باتفاق مسبق مع العميل لتوفير نتائج دقيقة تخدم متطلباته الخاصة",
-    },
-    {
-      title: "المحتوى المحلي",
-      desc: "دعم المنشآت في احتساب وتطوير المحتوى المحلي بما يتماشى مع أهداف رؤية المملكة 2030",
-    },
-  ];
-
   return (
-    <section className="bg-gray-100">
+    <div>
       <Pane
-      className="mb-14"
         title="خدماتنا"
         details="عملكم محل اهتمامنا لذلك نقدم لكم مجموعة من الخدمات المتميزة لنساهم في تنمية أعمالكم"
       />
-
-      <div className="max-w-7xl mx-auto xl:px-35">
-        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
-          {services.map((service, index) => (
-            <div
-              key={index}
-              className="
-              relative
-              w-72.5
-              h-72.5
-              bg-[#E7E3D4]
-              border-4
-              border-[#6B6A55]
-              rounded-[50%]
-              shadow-xl/30
-              flex
-              items-center
-              justify-center
-              text-center
-              p-6
-              "
-            >
-              <div>
-                <h3 className="text-xl font-bold text-[#6B6A55] mb-3">
-                  {service.title}
-                </h3>
-
-                <p className="text-sm text-gray-700 leading-relaxed">
-                  {service.desc}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+      <OurServices />
+    </div>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
-import Hero from "./components/Hero"
-import AboutUs from "./components/pages/AboutUs"
-import WhyChooseUs from "./components/pages/WhyChooseUs"
+import Hero from "./components/Hero";
+import AboutUs from "./components/pages/AboutUs";
+import OurServices from "./components/pages/OurServices";
+import WhyChooseUs from "./components/pages/WhyChooseUs";
 
 function page() {
   return (
     <main>
-      <Hero/>
-      <AboutUs/>
-      <WhyChooseUs/>
+      <Hero />
+      <AboutUs />
+      <WhyChooseUs />
+      <div className="relative">
+        <OurServices className="pt-50" />
+        <div className="text-center absolute top-20 left-0 right-0 ">
+          <h2 className="text-3xl md:text-4xl font-bold text-[#6B6A55] mb-4">
+            خدماتنا
+          </h2>
+
+          <p className="text-gray-600 max-w-2xl mx-auto">
+            عملكم محل اهتمامنا لذلك نقدم لكم مجموعة من الخدمات المتميزة لنساهم
+            في تنمية أعمالكم
+          </p>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default page
+export default page;
