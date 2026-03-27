c51b3142704a44d50ddc5c57725f522580ce5134
omarhassan <omarhassan.mansour@gmail.com>
2026-03-27 15:29:08 +0200

    Feat : edit some animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 import Hero from "./components/Hero";
 import AboutUs from "./components/pages/AboutUs";
 import OurServices from "./components/pages/OurServices";
@@ -12,17 +16,28 @@ function page() {
       <div className="relative">
         <OurServices className="pt-50" />
         <div className="text-center absolute top-20 left-0 right-0 ">
-          <h2 className="text-3xl md:text-4xl font-bold text-[#6B6A55] mb-4">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.6 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+            className="text-3xl md:text-4xl font-bold text-[#6B6A55] mb-4"
+          >
             خدماتنا
-          </h2>
+          </motion.div>
 
-          <p className="text-gray-600 max-w-2xl mx-auto">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            viewport={{ once: true }}
+            className="text-gray-600 max-w-2xl mx-auto"
+          >
             عملكم محل اهتمامنا لذلك نقدم لكم مجموعة من الخدمات المتميزة لنساهم
             في تنمية أعمالكم
-          </p>
+          </motion.div>
         </div>
       </div>
-      
     </main>
   );
 }
